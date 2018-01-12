b48457f9c5eff22f035b2c77e6c86865bd343903
xiaoxiao <277542324@qq.com>
2018-01-12 16:02:31 +0800

    增加新的判断状态字体颜色状态

--- app/src/main/java/com/xiaoxiao/utilis/MainActivity.java
+++ app/src/main/java/com/xiaoxiao/utilis/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //UtilsTools.StatusBarLightMode(this);
+        //UtilsTools.StatusBarLightMode(this,false);
     }
 }
